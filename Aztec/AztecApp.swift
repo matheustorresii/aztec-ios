ad6905cb539a6178d144ee50df99baa3de84190d
Matheus Torres <matheustf2003@gmail.com>
2023-08-08 02:15:46 -0300

    Initial Commit

---
+++ Aztec/AztecApp.swift
@@ -0,0 +1,17 @@
+//
+//  AztecApp.swift
+//  Aztec
+//
+//  Created by Matheus Torres on 08/08/23.
+//
+
+import SwiftUI
+
+@main
+struct AztecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
