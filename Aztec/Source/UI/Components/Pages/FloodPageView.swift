7b5b3ae71f1a6baf1e3e4f41c8ac3367b1fd021e
Matheus Torres <matheustf2003@gmail.com>
2023-08-14 04:06:13 -0300

    Create flooded page view

--- Aztec/Source/UI/Components/Pages/FloodPageView.swift
+++ Aztec/Source/UI/Components/Pages/FloodPageView.swift
@@ -6,26 +6,45 @@
 //
 
 import SwiftUI
+import SpriteKit
 
 struct FloodPageView: View {
     @State private var planetAnimated: Bool = false
     
+    private var scene: SKScene {
+        let scene = RainScene()
+        scene.size = UIScreen.main.bounds.size
+        scene.scaleMode = .fill
+        scene.backgroundColor = .clear
+        return scene
+    }
+    
     var body: some View {
-        Image("planet-flooded")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .padding(50)
-            .rotationEffect(planetAnimated ? .degrees(-2) : .degrees(2))
-            .onAppear {
-                withAnimation(.linear(duration: 1).repeatForever()) {
-                    planetAnimated.toggle()
+        ZStack {
+            SpriteView(scene: scene, options: [.allowsTransparency])
+                .ignoresSafeArea()
+            Image("planet-flooded")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(50)
+                .rotationEffect(planetAnimated ? .degrees(-2) : .degrees(2))
+                .onAppear {
+                    withAnimation(.linear(duration: 1).repeatForever()) {
+                        planetAnimated.toggle()
+                    }
                 }
-            }
+        }
     }
 }
 
-struct FloodPageView_Previews: PreviewProvider {
-    static var previews: some View {
-        FloodPageView()
+class RainScene: SKScene {
+    let rainEmitter: SKEmitterNode? = .init(fileNamed: "Rain.sks")
+    
+    override func didMove(to view: SKView) {
+        guard let rainEmitter else { return }
+        addChild(rainEmitter)
+        rainEmitter.position.y = self.frame.maxY
+        rainEmitter.particlePositionRange.dx = self.frame.width * 2.5
+        view.allowsTransparency = true
     }
 }
